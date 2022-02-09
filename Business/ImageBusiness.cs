a7baa22cad04efb2c361a7346b06ddecfe7bed03
Saeed Nemati <sn.1361@gmail.com>
2022-02-09 16:34:12 +0330

    committed by script

--- Business/ImageBusiness.cs
+++ Business/ImageBusiness.cs
@@ -5,4 +5,9 @@ public class ImageBusiness : Business<ImageView, Image>
     protected override ReadRepository<ImageView> ReadRepository => Repository.ImageView;
 
     protected override Repository<Image> WriteRepository => Repository.Image;
+
+    protected override void ModifyItemBeforeReturning(ImageView image)
+    {
+        base.ModifyItemBeforeReturning(image);
+    }
 }
