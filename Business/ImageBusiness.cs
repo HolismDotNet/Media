718f15741ad62bf399389ac6a0a6873d9fcf63ab
Saeed Nemati <sn.1361@gmail.com>
2022-02-09 20:58:43 +0330

    committed by script

--- Business/ImageBusiness.cs
+++ Business/ImageBusiness.cs
@@ -6,8 +6,16 @@ public class ImageBusiness : Business<ImageView, Image>
 
     protected override Repository<Image> WriteRepository => Repository.Image;
 
+    public string GetContainerName(string entityType)
+    {
+        return $"{entityType.ToLower()}images";
+    }
+
     protected override void ModifyItemBeforeReturning(ImageView image)
     {
+        var entityType = new EntityTypeBusiness().GetName(image.EntityTypeGuid);
+        image.RelatedItems.OriginalEntityType = entityType;
+        image.RelatedItems.Url = Storage.GetImageUrl(GetContainerName(entityType), image.ImageGuid);
         base.ModifyItemBeforeReturning(image);
     }
 
@@ -17,7 +25,7 @@ public class ImageBusiness : Business<ImageView, Image>
         image.EntityTypeGuid = new EntityTypeBusiness().GetGuid(entityType);
         image.EntityGuid = entityGuid;
         image.ImageGuid = Guid.NewGuid();
-        Storage.UploadImage(bytes, image.ImageGuid, entityType.ToLower() + "images");
+        Storage.UploadImage(bytes, image.ImageGuid, GetContainerName(entityType));
         Create(image);
         return Get(image.Id);
     }
